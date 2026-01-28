9df3d97a5158c5e29bf3047bb371eda4b59932e4
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-01-29 00:28:54 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import HackathonRegistration from './components/HackathonRegistration';
 import MasterclassRegistration from './components/MasterclassRegistration';
+import GFGRegistration from './components/GFGRegistration';
 import Background from './components/Background';
 import { AnimationProvider } from './context/AnimationContext';
 import './App.css';
@@ -11,12 +13,13 @@ function App() {
     <Router>
       <AnimationProvider>
         <div className="app-container">
+          <Navbar />
           <Background />
-          {/* We can add a Navbar here later if needed */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register/hackathon" element={<HackathonRegistration />} />
             <Route path="/register/masterclass" element={<MasterclassRegistration />} />
+            <Route path="/register/gfg" element={<GFGRegistration />} />
           </Routes>
         </div>
       </AnimationProvider>
